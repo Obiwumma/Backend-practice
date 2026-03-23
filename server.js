bf9c7511082d6dfe764da4271837d53c2d0e299a
Obiwumma Pascal <pobiwumma@gmail.com>
2026-03-23 21:07:22 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+// 1. Import the express library
+const express = require('express');
+
+// 2. Initialize the application
+const app = express();
+
+// 3. Define a "Port" (The door the server listens on)
+const PORT = 3000;
+
+// 4. Create your first Route (GET request to the home path '/')
+app.get('/', (req, res) => {
+  // req = Request (what the client sent)
+  // res = Response (what we send back)
+  res.status(200).json({
+    message: "Welcome to my first production API!",
+    status: "Success"
+  });
+});
+
+app.get('/book', (req, res) => {
+  res.status(200).json({
+    "name": "The way of Kings",
+    "author": "Spielberg",
+    "tags": ["Fantasy", "Dark", "Adventure"],
+    "stock": {
+        "quantity": 23,
+        "isAvailable": true
+      }
+  })
+})
+
+// 5. Start the server
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running at http://localhost:${PORT}`);
+});
