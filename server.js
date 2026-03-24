d112cd0ae10a0cada62ac2e1caa4f5235b01c361
Obiwumma Pascal <pobiwumma@gmail.com>
2026-03-24 22:09:03 +0100

    Add admin route

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ const app = express();
 // 3. Define a "Port" (The door the server listens on)
 const PORT = 3000;
 
+const bookData = {
+  "name": "The way of Kings",
+  "author": "Spielberg",
+  "tags": ["Fantasy", "Dark", "Adventure"],
+  "stock": {
+      "quantity": 23,
+      "isAvailable": true
+    }
+}
+
 // 4. Create your first Route (GET request to the home path '/')
 app.get('/', (req, res) => {
   // req = Request (what the client sent)
@@ -18,15 +28,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/book', (req, res) => {
-  res.status(200).json({
-    "name": "The way of Kings",
-    "author": "Spielberg",
-    "tags": ["Fantasy", "Dark", "Adventure"],
-    "stock": {
-        "quantity": 23,
-        "isAvailable": true
-      }
-  })
+  res.status(200).json(bookData)
+})
+app.get('/admin', (req, res) => {
+  res.status(403).json("Access Denied")
 })
 
 // 5. Start the server
