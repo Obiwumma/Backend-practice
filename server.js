0bd401acb45ea28d1db4da23c52b974f520a8d9d
Obiwumma Pascal <pobiwumma@gmail.com>
2026-03-30 02:08:11 +0100

    Created my own middleware function

--- server.js
+++ server.js
@@ -19,6 +19,42 @@ const bookData = {
     }
 }
 
+// This is a custom middleware function
+app.use((req, res, next) => {
+  const time = new Date().toLocaleTimeString();
+  console.log(`[${time}] ${req.method} request to ${req.url}`);
+  
+  // CRITICAL: You must call next() or the request will hang forever!
+  next(); 
+});
+
+const gatekeeper = (req, res, next) => {
+  const adminKey = req.get('admin-key');
+  
+  // Use the secret from your .env file
+  const validKey = process.env.ADMIN_SECRET || "12345"; 
+
+  if (adminKey === validKey) {
+    return next(); // 'return' ensures no other code in this function runs
+  }
+
+  // Providing a clear reason for the failure is great for API consumers
+  return res.status(403).json({ 
+    success: false,
+    message: "Access Denied: Invalid or missing admin-key" 
+  });
+};
+
+
+// Built-in Middleware to handle JSON data from clients
+app.use(express.json());
+
+// Another middleware
+app.get('/secret-vault', gatekeeper, (req, res) => {
+
+  res.status(200).json({ "data": "This is top secret gold!" })
+})
+
 // 4. Create your first Route (GET request to the home path '/')
 app.get('/', (req, res) => {
   // req = Request (what the client sent)
